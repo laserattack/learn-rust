d2661e4825d21a16e8e906f561207badcd0a7955
serr <sc7227484@gmail.com>
2026-04-09 21:47:23 +0300

    heap

--- heap.rs
+++ heap.rs
@@ -4,8 +4,31 @@
 // rustc -o heap.out heap.rs && ./heap.out && rm -f heap.out
 
 fn main() {
-    let mut val = Box::<u32>::new_uninit(); // box - значение в куче
-    val.write(5);
-    val.write(10);
-    val.write(1337);
+    let big_box = Box::<[usize; 1024]>::new_uninit();
+
+    let mut array = [0; 1024];
+    for (i, place) in array.iter_mut().enumerate() {
+        *place = i;
+    }
+
+    // file:///home/serr/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/std/boxed/struct.Box.html#method.write
+    // Оптимизатор может исключить это копирование, так что предыдущий код (цикл) будет писать прямо в кучу.
+    let big_box = Box::write(big_box, array);
+
+    for (i, x) in big_box.iter().enumerate() {
+        assert_eq!(*x, i);
+    }
+
+    // гарантированно писать в сразу кучу можно так (приходится использовать unsafe)
+    let mut big_box = Box::<[usize; 1024]>::new_uninit();
+    let ptr = big_box.as_mut_ptr() as *mut usize;
+    for i in 0..1024 {
+        unsafe { ptr.add(i).write(i); }
+    }
+    let big_box = unsafe { big_box.assume_init() };
+    for (i, x) in big_box.iter().enumerate() {
+        assert_eq!(*x, i);
+    }
+
+    // ну или просто использовать вектор!
 }
